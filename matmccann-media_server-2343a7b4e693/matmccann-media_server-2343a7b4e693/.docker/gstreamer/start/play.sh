d39efcff2c344cd48aa1a6ad3e8dbce592c23cb1
Emran Billah <omegaregions0072004@yahoo.com>
2023-02-09 14:29:53 -0400

    In working state

--- matmccann-media_server-2343a7b4e693/matmccann-media_server-2343a7b4e693/.docker/gstreamer/start/play.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-export VID=/start/sample.mp4
-export AMS_URL='rtmp://172.23.0.2:1935/WebRTCApp/test-001'
-gst-launch-1.0 videotestsrc ! videoconvert ! x264enc ! flvmux ! rtmpsink location=$AMS_URL
--- matmccann-media_server-2343a7b4e693/matmccann-media_server-2343a7b4e693/.docker/media_server/systemd_daemon/readme.md
+++
@@ -1,2 +0,0 @@
-Copy the .service and .socket files to location: /etc/systemd/system
-to enable systemd daemon
